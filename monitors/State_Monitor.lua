b23c8a730788b2c0ef31b0b64c2cbb4e3cfe67ed
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2023-02-20 14:52:39 -0800

    Fix Area Zone and WM Zone being swapped

--- monitors/State_Monitor.lua
+++ monitors/State_Monitor.lua
@@ -53,9 +53,9 @@ end
 function StateMonitor:read()
   local buffer = mainmemory.read_bytes_as_array(Address.GAMESTATE_BASE, 16)
   local partySize = buffer[4]
-  self:updateState("WM_ZONE", buffer[1])
+  self:updateState("AREA_ZONE", buffer[1])
   self:updateState("SCREEN_ZONE", buffer[2])
-  self:updateState("AREA_ZONE", buffer[3])
+  self:updateState("WM_ZONE", buffer[3])
   self:updateState("RNG", mainmemory.read_u32_le(Address.RNG))
 
   self:updateState("IG_CURRENT_GAMESTATE", mainmemory.read_u8(Address.GAMESTATE))
@@ -72,89 +72,5 @@ end
 function StateMonitor:init()
   self:read()
 end
-
-
--- function BattlesHUD:updateState()
---   if self.Locked then return end
-
---   local location = EncounterLib.onWorldMapOrOverworld()
-
---   if location ~= Gamestates.WORLD_MAP and location ~= Gamestates.OVERWORLD then
---     return false
---   end
-
---   local wm_zone = memory.read_u8(Address.WM_ZONE)
---   local area_zone = memory.read_u8(Address.AREA_ZONE)
---   local inGameEncounterRate = memory.read_u8(Address.ENCOUNTER_RATE)
-
---   -- Get Champion Rune Info
---   local IsChampion = PartyLib.isChampionsRuneEquipped()
---   local PartyLevel = PartyLib.getPartyLVL()
-
---   local stateChanged = false
-
---   -- print("Location:", location, self.State.Location)
---   -- print("WM Zone:", wm_zone, self.State.WM_Zone)
---   -- print("Area Zone:", area_zone, self.State.Area_Zone)
---   -- print("Encounter Rate:", inGameEncounterRate, self.State.EncounterRate)
-
---   if location ~= self.State.Location then
---     -- print("Location changed")
---     stateChanged = true
---   elseif wm_zone ~= self.State.WM_Zone then
---     -- print("WM Zone changed")
---     stateChanged = true
---   elseif area_zone ~= self.State.Area_Zone then
---     -- print("Area Zone changed")
---     stateChanged = true
---   elseif inGameEncounterRate ~= self.State.EncounterRate then
---     -- print("EncounterRate changed")
---     stateChanged = true
---   elseif IsChampion then
---     if not self.State.IsChampion then
---       stateChanged = true
---     elseif PartyLevel ~= self.State.PartyLevel then
---       stateChanged = true
---     end
---   end
-
---   -- Check if change, if not do nothing.
---   if not stateChanged then
---     return false
---   end
-
---   local encounterRate
---   local name
---   local data
-
---   if location == Gamestates.WORLD_MAP then
---     name = ZoneInfo[wm_zone].name
---     data = EncounterTable[name]
---     if not data then return false end
---     encounterRate = 8
---   elseif location == Gamestates.OVERWORLD then
---     name = ZoneInfo[wm_zone][area_zone]
---     data = EncounterTable[name]
---     if not data then return false end
---     encounterRate = math.min(inGameEncounterRate, data.encounterRate)
---   end
-
-
---   self.State = {
---     Location = location,
---     Name = name,
---     EncounterTable = data.encounters,
---     Enemies = data.enemies,
---     EncounterRate = encounterRate,
---     EncounterTableSize = #data.encounters,
---     ChampVals = data.champVals,
---     WM_Zone = wm_zone,
---     Area_Zone = area_zone,
---     IsChampion = IsChampion,
---     PartyLevel = PartyLevel
---   }
-
---   return true
--- end
 
 return StateMonitor
