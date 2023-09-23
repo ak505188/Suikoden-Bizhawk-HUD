d2637f8c9ee531a4290a66da8e13e6605a5839a2
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2023-09-23 14:54:04 -0700

    Typo fix

--- controllers/monitors.lua
+++ controllers/monitors.lua
@@ -1,17 +1,3 @@
-local StateMonitor = require "monitors.State_Monitor"
-local RNGMonitor = require "monitors.RNG_Monitor"
-local MenuController = require "menus.MenuController"
-local RNGResetMenu = require "menus.RNG_Reset_Menu"
-
-StateMonitor:init()
-RNGMonitor:init()
-
-local MonitorsController = {
-  monitors = { RNGMonitor, StateMonitor },
-  RNG = RNGMonitor,
-  State = StateMonitor,
-}
-
 function MonitorsController:init()
   for _,monitor in ipairs(self.monitors) do
     monitor:init()
